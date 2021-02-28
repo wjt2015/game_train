fee2609a81b7bd7c80894bf1c8b4783c962d5fa9
unknown <15236286736@163.com>
2021-02-28 12:36:47 +0800

    update pom

--- src/main/java/wjt/game/App.java
+++ src/main/java/wjt/game/App.java
@@ -1,5 +1,8 @@
 package wjt.game;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String s = Joiner.on("-").join(Lists.newArrayList(1, 2, 3, 4, 5));
+        System.out.println("s="+s);
     }
 }
